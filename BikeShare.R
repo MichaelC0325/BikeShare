349bcefd0fd84e4ef6d2cfa1ff6711f7e687a128
Michael Chang <michael.chang0325@gmail.com>
2024-09-16 19:00:23 -0600

    Feature Engineering

--- BikeShare.R
+++ BikeShare.R
@@ -35,8 +35,54 @@ weather_plot <- ggplot(data, aes(x = weather)) +
 
 
 # -------------------------------------------------------------------------
+trainData <- vroom("train.csv")
+testData <- vroom("test.csv")
 
-summary(dataset)
+#feature engineering
+trainData <- trainData |> 
+  select(-casual, -registered) |> 
+  mutate(count = log(count))
+
+my_recipe <- recipe(count~., data = trainData) |> 
+  step_mutate(weather = ifelse(weather == 4, 3, weather)) |> 
+  step_mutate(weather = factor(weather), levels = 3) |> 
+  step_time(datetime, features = c("hour", "minute")) |> 
+  step_mutate(season=factor(season, levels= 4)) |> 
+  step_zv(all_predictors()) |> 
+  step_mutate(holiday = factor(holiday), levels = 2) |>
+  step_mutate(workingday = factor(workingday), levels = 2) |> 
+  step_poly(windspeed, degree=2)
+
+
+bike_recipe <- prep(my_recipe) # Sets up the preprocessing using myDataSet
+bake(prepped_recipe, new_data=trainData)
+
+
+## Define a Model
+lin_model <- linear_reg() %>%
+set_engine("lm") %>%
+set_mode("regression")
+
+# Combine into a Workflow and fit
+bike_workflow <- workflow() %>%
+  add_recipe(my_recipe) %>%  # Add the untrained recipe here
+  add_model(lin_model) %>%
+  fit(data = trainData)  # Fit with training data
+
+
+## Run all the steps on test data
+lin_preds <- predict(bike_workflow, new_data = testData)
+lin_preds = exp(lin_preds)
+
+## Format the Predictions for Submission to Kaggle
+kaggle_submission_feature_engineering <- lin_preds %>%
+  bind_cols(., testData) %>% #Bind predictions with test data
+  select(datetime, .pred) %>% #Just keep datetime and prediction va
+  rename(count=.pred) %>% #rename pred to count (for submission to
+  mutate(datetime=as.character(format(datetime))) #needed for right
+
+## Write out the file
+vroom_write(x=kaggle_submission_feature_engineering, file="./FeatureEngineeringPreds4.csv", delim=",")
 
 
 # -------------------------------------------------------------------------
