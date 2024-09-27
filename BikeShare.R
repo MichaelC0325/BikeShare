2efdc173986c872ecdbf1e387dd1f29670c366f7
Michael Chang <michael.chang0325@gmail.com>
2024-09-27 14:19:28 -0600

    Stacking

--- BikeShare.R
+++ BikeShare.R
@@ -35,7 +35,7 @@ weather_plot <- ggplot(data, aes(x = weather)) +
 (weather_plot + temp_plot) / (windspeed_plot + humidity_plot)
 
 
-# feature engineering -----------------------------------------------------
+# feature engineering / linear regression -----------------------------------------------------
 
 
 trainData <- vroom("train.csv")
@@ -68,8 +68,8 @@ set_engine("lm") %>%
 set_mode("regression")
 
 # Combine into a Workflow and fit
-bike_workflow <- workflow() %>%
-  add_recipe(my_recipe) %>%  # Add the untrained recipe here
+linear_wf <- workflow() %>%
+  add_recipe(my_recipe_stack) %>%  # Add the untrained recipe here
   add_model(lin_model) %>%
   fit(data = trainData)  # Fit with training data
 
@@ -124,8 +124,8 @@ penalized_model <- linear_reg(penalty=.01, mixture=0) %>% #Set model and tuning
   set_engine("glmnet")# Function to fit in R
 
 
-preg_wf <- workflow() %>%
-add_recipe(my_recipe) %>%
+penalized_wf <- workflow() %>%
+add_recipe(my_recipe_stack) %>%
 add_model(penalized_model) %>%
 fit(data=trainData)
 
@@ -262,7 +262,7 @@ kaggle_submission_feature_engineering <- tree_preds %>%
 vroom_write(x=kaggle_submission_feature_engineering, file="./TreePreds.csv", delim=",")
 
 
-# Regression Trees --------------------------------------------------------
+# Random Forests --------------------------------------------------------
 
 library(tidymodels)
 
@@ -283,14 +283,14 @@ my_mod <- rand_forest(mtry = tune(),
 
 my_recipe <- recipe(count~., data = trainData) |> 
   step_mutate(weather = ifelse(weather == 4, 3, weather)) |> 
-  step_mutate(weather = factor(weather), levels = 3) |> 
-  step_time(datetime, features = c("hour", "minute")) |> 
-  step_mutate(season=factor(season, levels= 4)) |> 
+  step_mutate(weather = factor(weather)) |> 
+  step_time(datetime, features = c("hour")) |> 
+  step_mutate(season=factor(season)) |> 
   step_zv(all_predictors()) |> 
-  step_mutate(holiday = factor(holiday), levels = 2) |>
-  step_mutate(workingday = factor(workingday), levels = 2) |> 
+  step_mutate(holiday = factor(holiday)) |>
+  step_mutate(workingday = factor(workingday)) |> 
   step_poly(windspeed, degree=2) |> 
-  step_mutate(datetime_hour = factor(datetime_hour), levels = 24) |> 
+  step_mutate(datetime_hour = factor(datetime_hour)) |> 
   step_rm(datetime)
 
 forest_wf <- workflow() %>%
@@ -318,25 +318,25 @@ folds <- vfold_cv(trainData, v = 5, repeats=1)
 ## Find best tuning parameters
 
 ## Run the CV1
-CV_results <- forest_wf %>%
+CV_results_forest <- forest_wf %>%
 tune_grid(resamples=folds,
           grid=grid_of_tuning_params,
           metrics=metric_set(rmse, mae, rsq)) #Or leave metrics NULL
 
 ## Find Best Tuning Parameters
-bestTune <- CV_results %>%
+bestTune <- CV_results_forest %>%
 select_best(metric = "rmse")
 
 ## Finalize workflow and predict
 
 # Finalize the Workflow & fit it
-final_wf <-
+final_wf_forest <-
 forest_wf %>%
 finalize_workflow(bestTune) %>%
 fit(data=trainData)
 
 ## Predict
-forest_preds <- final_wf %>%
+forest_preds <- final_wf_forest %>%
 predict(new_data = testData)
 
 forest_preds <- exp(forest_preds)
@@ -350,3 +350,137 @@ kaggle_submission_feature_engineering <- forest_preds %>%
 
 ## Write out the file
 vroom_write(x=kaggle_submission_feature_engineering, file="./ForestPreds.csv", delim=",")
+
+# Stacking ----------------------------------------------------------------
+
+library(stacks) # you need this library to create a stacked model
+
+trainData <- vroom("train.csv")
+testData <- vroom("test.csv")
+
+trainData <- trainData |> 
+  select(-casual, -registered) |> 
+  mutate(count = log(count))
+
+my_recipe_stack <- recipe(count~., data = trainData) |> 
+  step_mutate(weather = ifelse(weather == 4, 3, weather)) |> 
+  step_mutate(weather = factor(weather)) |> 
+  step_time(datetime, features = c("hour")) |> 
+  step_mutate(season=factor(season)) |> 
+  step_mutate(holiday = factor(holiday)) |>
+  step_mutate(workingday = factor(workingday)) |> 
+  step_mutate(datetime_hour = factor(datetime_hour)) |> 
+  step_rm(datetime) |> 
+  step_zv(all_predictors()) |> 
+  step_dummy(all_nominal_predictors())
+
+
+preped <- prep(my_recipe_stack)
+baked <- bake(preped, new_data = testData)
+baked
+
+## Split data for CV
+folds <- vfold_cv(trainData, v = 5, repeats=1)
+
+## Create a control grid
+untunedModel <- control_stack_grid() #If tuning over a grid
+tunedModel <- control_stack_resamples() #If not tuning a model
+
+## Penalized regression model
+preg_model <- linear_reg(penalty=tune(),
+                         mixture=tune()) %>% #Set model and tuning
+  set_engine("glmnet") # Function to fit in R
+
+## Set Workflow
+preg_wf <- workflow() %>%
+  add_recipe(my_recipe_stack) %>%
+  add_model(preg_model)
+
+## Set up grid of tuning values
+
+tuning_grid <- grid_regular(penalty(),
+                            mixture(),
+                            levels = 5) ## L^2 total tuning possibilities
+
+# Run cross-validation for penalized regression model
+preg_models <- preg_wf %>%
+  tune_grid(
+    resamples = folds,
+    grid = tuning_grid,
+    metrics = metric_set(rmse),
+    control = untunedModel  # This should be defined as control_grid(save_pred = TRUE)
+  )
+
+# Linear regression workflow and resampling
+lin_reg <- linear_reg() %>%
+  set_engine("lm")
+
+lin_reg_wf <- workflow() %>%
+  add_model(lin_reg) %>%
+  add_recipe(my_recipe_stack)
+
+lin_reg_model <- fit_resamples(
+  lin_reg_wf,
+  resamples = folds,
+  metrics = metric_set(rmse),
+  control = tunedModel  # This should be defined as control_resamples(save_pred = TRUE)
+)
+
+rf_model <- rand_forest(mtry = tune(), # Number of predictors to consider at each split
+                        trees = 500,   # Number of trees
+                        min_n = tune()) %>% # Minimum number of samples in a node
+  set_engine("ranger") %>%   # Using the "ranger" engine for Random Forests
+  set_mode("regression")     # Set the mode for regression (can be "classification" if needed)
+
+## Create a new workflow for Random Forest
+rf_wf <- workflow() %>%
+  add_model(rf_model) %>%
+  add_recipe(my_recipe_stack)  # Include your preprocessing recipe
+
+# Define a grid for tuning hyperparameters (adjust levels as needed)
+rf_tuning_grid <- grid_regular(
+  mtry(range = c(1, ncol(trainData) - 1)), # Tune over the number of predictors considered at each split
+  min_n(range = c(2, 10)),                 # Tune over minimum node size
+  levels = 5                               # Number of values to evaluate for each parameter
+)
+
+# Perform grid search tuning using cross-validation
+rf_model_results <- rf_wf %>%
+  tune_grid(
+    resamples = folds,                    # Use your predefined resampling folds
+    grid = rf_tuning_grid,                # Pass the tuning grid
+    metrics = metric_set(rmse),           # Specify metrics for evaluation
+    control = control_grid(save_pred = TRUE, save_workflow = TRUE)    
+  )
+
+
+
+# Create the stack and add candidates
+my_stack <- stacks() %>%
+  add_candidates(lin_reg_model) %>%
+  add_candidates(preg_models) %>%
+  add_candidates(rf_model_results)  
+
+# Fit the stacked model
+stack_mod <- my_stack %>%
+  blend_predictions() %>%  # Blend predictions using LASSO or another method
+  fit_members()            # Fit individual members of the stacked ensemble
+
+
+## If you want to build your own metalearner you'll have to do so manually
+## using
+stackData <- as_tibble(my_stack)
+
+## Use the stacked data to get a prediction
+stacking_predictions <- stack_mod %>% predict(new_data=testData)
+stacking_predictions = exp(stacking_predictions)
+
+## Format the Predictions for Submission to Kaggle
+kaggle_submission_feature_engineering <- stacking_predictions %>%
+  bind_cols(., testData) %>% #Bind predictions with test data
+  select(datetime, .pred) %>% #Just keep datetime and prediction va
+  rename(count=.pred) %>% #rename pred to count (for submission to
+  mutate(datetime=as.character(format(datetime))) #needed for right
+
+## Write out the file
+vroom_write(x=kaggle_submission_feature_engineering, file="./StackingPreds3.csv", delim=",")
