d02f48560c8e254688c341f21550827f57118dca
Michael Chang <michael.chang0325@gmail.com>
2024-09-30 10:59:55 -0600

    Bart

--- BikeShare.R
+++ BikeShare.R
@@ -484,3 +484,61 @@ kaggle_submission_feature_engineering <- stacking_predictions %>%
 
 ## Write out the file
 vroom_write(x=kaggle_submission_feature_engineering, file="./StackingPreds3.csv", delim=",")
+
+# Bart --------------------------------------------------------------------
+
+trainData <- vroom("train.csv")
+testData <- vroom("test.csv")
+
+
+trainData <- trainData |> 
+  select(-casual, -registered) |> 
+  mutate(count = log(count))
+
+my_recipe_bart <- recipe(count~., data = trainData) |> 
+  step_mutate(weather = ifelse(weather == 4, 3, weather)) |> 
+  step_mutate(weather = factor(weather)) |> 
+  step_time(datetime, features = c("hour")) |> 
+  step_mutate(season = factor(season)) |> 
+  step_mutate(holiday = factor(holiday)) |>
+  step_mutate(workingday = factor(workingday)) |> 
+  step_mutate(datetime_hour = factor(datetime_hour)) |> 
+  step_rm(datetime) |> 
+  step_poly(all_numeric_predictors(), degree = 2) |> 
+  step_interact(~ all_predictors():all_predictors()) |> 
+  step_zv(all_predictors()) |> 
+  step_dummy(all_nominal_predictors()) |> 
+  step_normalize(all_numeric_predictors())
+
+
+#preped <- prep(my_recipe_bart)
+#baked <- bake(preped, new_data = testData)
+#baked
+
+
+
+bart_model <- parsnip::bart(trees = 50) |> 
+  set_engine("dbarts") |> 
+  set_mode("regression")
+
+
+# Create a workflow to combine the recipe and model specification
+bart_workflow <- workflow() %>%
+  add_recipe(my_recipe_bart) %>%
+  add_model(bart_model) |> 
+  fit(data = trainData)
+
+# Fit the BART model to the training data
+bart_predictions <- predict(bart_workflow, new_data = testData)
+bart_predictions <- exp(bart_predictions)
+
+## Format the Predictions for Submission to Kaggle
+kaggle_submission_bart <- bart_predictions %>%
+  bind_cols(., testData) %>% #Bind predictions with test data
+  select(datetime, .pred) %>% #Just keep datetime and prediction va
+  rename(count=.pred) %>% #rename pred to count (for submission to
+  mutate(datetime=as.character(format(datetime))) #needed for right
+
+## Write out the file
+vroom_write(x=kaggle_submission_bart, file="./BartPreds9.csv", delim=",")
+
